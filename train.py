ee4f58a110c3c1b66af2c4906fb8dc6651314c8f
Torfinhell <nikita2school@icloud.com>
2026-03-02 18:39:11 +0300

    vocab size changed

--- train.py
+++ train.py
@@ -17,7 +17,6 @@ def train(training_config: TrainingConfig, model, dl_train, dl_val, vocab):
 
     gradient_accumulation_steps = training_config.GRAD_ACUM
     best_bleu = 0.0
-
     steps_per_epoch = (len(dl_train) + gradient_accumulation_steps - 1) // gradient_accumulation_steps
     total_optimizer_steps = steps_per_epoch * training_config.NUM_EPOCHS
 
