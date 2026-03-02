13657bb1c1b8b533d51f69158dc4a36c168986cc
Torfinhell <nikita2school@icloud.com>
2026-03-02 10:55:28 +0300

    added pos_emb

--- modules/config.py
+++ modules/config.py
@@ -3,30 +3,33 @@ import torch
 
 @dataclass
 class ModelConfig:
-    NUM_HEADS: int = 8
+    NUM_HEADS: int = 12
     DIM_KV: int = 64
-    DIM_MODEL: int = 512
-    EPS_LAYER_NORM: float = 1e-8
-    D_FF: int = 1024
-    NUM_ENCODER_LAYERS: int = 4
-    NUM_DECODER_LAYERS: int = 4
+    DIM_MODEL: int = 768
+    EPS_LAYER_NORM: float = 1e-6
+    D_FF: int = 3072
+    NUM_ENCODER_LAYERS: int = 12
+    NUM_DECODER_LAYERS: int = 12
     MAX_SEQ_LEN: int = 512
     DROPOUT: float = 0.1
-    VOCAB_SIZE: int = 0
+    VOCAB_SIZE: int = 32128
     PAD_TOKEN_ID: int = 0
-    BOS_TOKEN_ID: int = 2
-    EOS_TOKEN_ID: int = 4
+    BOS_TOKEN_ID: int = 0
+    EOS_TOKEN_ID: int = 1
+    RELATIVE_ATTENTION_NUM_BUCKETS: int = 32
+
 @dataclass
 class TrainingConfig:
-    BATCH_SIZE: int = 40
+    BATCH_SIZE: int = 16
     DEVICE: torch.device = torch.device(
         'cuda:0' if torch.cuda.is_available() else 'cpu'
     )
-    DATA_FOLDER:str="data"
-    NUM_EPOCHS:int=100
-    LR:float=1e-4
-    TRAIN_EPOCH_LEN:int=None
-    GRAD_ACUM:int=1
+    DATA_FOLDER: str = "data"
+    NUM_EPOCHS: int = 100
+    LR: float = 1e-4
+    TRAIN_EPOCH_LEN: int = None
+    GRAD_ACUM: int = 4
+
 @dataclass
 class InferenceConfig:
     pass
